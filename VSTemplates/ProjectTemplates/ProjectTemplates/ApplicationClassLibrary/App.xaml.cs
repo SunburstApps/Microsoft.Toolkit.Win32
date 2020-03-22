82ebef46d046de9fb8014c4674be0fd2eb8f5fa7
William Kent <wjk@users.noreply.github.com>
2020-03-21 20:54:14 -0400

    Add templates to vsixmanifest file

---
+++ VSTemplates/ProjectTemplates/ProjectTemplates/ApplicationClassLibrary/App.xaml.cs
@@ -0,0 +1,18 @@
+using System;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace $rootnamespace$
+{
+    sealed partial class App
+    {
+        public App()
+        {
+            this.Initialize();
+            this.InitializeComponent();
+        }
+    }
+}
